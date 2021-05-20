cf1e148708de8ce0136834b77a09db49b9335ecf
ZardoxGit <sounouk@hotmail.fr>
2021-05-20 11:51:17 +0200

    fin exo 3

--- script.js
+++ script.js
@@ -9,3 +9,47 @@ for(var i=0; i<document.getElementsByClassName('trash').length; i++){
       document.getElementById('count').textContent = numberOfMessages;
     })
 }
+
+document.getElementById('addButton').addEventListener('click',
+function() {
+  
+  var newDiv = document.createElement('div');
+      newDiv.className = 'row';
+      document.body.appendChild(newDiv);
+
+  var image = document.createElement('img');
+      image.className = 'avatar';
+      image.src = 'avatar-1.jpg';
+      newDiv.appendChild(image);
+
+  var messageDiv = document.createElement('div');
+      newDiv.appendChild(messageDiv);
+
+  var h6 = document.createElement('h6');
+      h6.textContent = 'Jacques Grandjean';
+      messageDiv.appendChild(h6);
+
+  var text = document.createElement('p');
+      text.textContent = document.getElementById('addMessage').value;
+      messageDiv.appendChild(text);
+
+  var trash = document.createElement('img');
+      trash.className = 'trash'
+      trash.src = 'trash.png';
+      newDiv.appendChild(trash);
+
+      document.getElementById('addMessage').value = '';
+
+  var numberOfMessages = document.getElementsByTagName('p').length;
+      document.getElementById('count').textContent = numberOfMessages;
+
+      trash.addEventListener('click',
+      function(){
+        this.parentNode.remove();
+
+  var numberOfMessages = document.getElementsByTagName('p').length;
+        document.getElementById('count').textContent = numberOfMessages;
+      })
+
+
+})
